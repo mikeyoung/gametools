b33ee9aaf9219284e8f97af302c28f181adb390e
mikeyoung <mike@mikeyoung.org>
2024-02-05 22:36:56 -0500

    set static root

--- gametools/settings.py
+++ gametools/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-+ok_qj5x5(aij*^$6zu_wk8rcb6cbwdwp%371pk&csg56q9eu_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mikeyoung.pythonanywhere.com']
+ALLOWED_HOSTS = ['mikeyoung.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "/gametools/mutantfuture/static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
