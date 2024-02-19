4450da8d7d456c5eaf2f11c1bc5c7a2d4947d9a3
mikeyoung <mike@mikeyoung.org>
2024-02-18 21:23:35 -0500

    got mutation modals working

--- mutantfuture/static/mutantfuture/script/mutations.js
+++ mutantfuture/static/mutantfuture/script/mutations.js
@@ -1,5 +1,10 @@
 const RULEBOOK_PATH = '/static/mutantfuture/json/rulebook.json';
 
+const get_mutation_by_pk = (rulebook, mutation_id) => {
+    let filtered_mutations = rulebook.mutations.filter(mutation => mutation.pk == mutation_id);
+    return filtered_mutations[0];
+};
+
 const mutations_main = async () => {
     const rulebook = await get_rulebook(RULEBOOK_PATH);
 
@@ -10,13 +15,41 @@ const mutations_main = async () => {
     });
 
     for (mutation of mutations) {
-        const liHtml = `<li><a id="mulink${mutation.pk}" href="javascript:void(0)" data-muid="${mutation.pk}">${mutation.fields.name} (${mutation.fields.type})</a></li>`;
+        const liHtml = `<li><a id="mulink${mutation.pk}" class="mutation-link" href="javascript:void(0)" data-muid="${mutation.pk}">${mutation.fields.name} (${mutation.fields.type})</a></li>`;
         document.querySelector('#mutations-list').innerHTML += liHtml;
-
-        document.querySelector(`#mulink${mutation.pk}`).addEventListener('click', () => {
-            console.log(111);
-        });
     }
+
+    document.querySelectorAll('.mutation-link').forEach((link) => {
+        link.addEventListener('click', (el) => {
+            
+
+
+
+
+
+            // instanciate new modal
+            let modal = new tingle.modal({
+                closeMethods: ['overlay', 'button', 'escape'],
+                closeLabel: "Close",
+                cssClass: ['custom-class-1']
+            });
+
+            let mutation = get_mutation_by_pk(rulebook, el.target.dataset.muid);
+            let modalContent = `<article>`;
+            modalContent += `<h3>${mutation.fields.name}</h3>`;
+            modalContent += mutation.fields.description;
+            modalContent = modalContent.replace(/<br\s*\/?>/gi, " ");
+            modalContent += `</article>`;
+            modal.setContent(modalContent);
+
+            modal.open();
+
+
+
+
+
+        });
+    })
 }
 
 document.addEventListener('DOMContentLoaded', mutations_main);
