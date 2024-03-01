5d1e4e20f0a74501417445978c3e24ef416faad5
Mike Young <mike@mikeyoung.org>
2024-02-29 22:32:33 -0500

    moved mf to subfolder

--- gametools/urls.py
+++ gametools/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("", include("mutantfuture.urls")),
+    path("mf/", include("mutantfuture.urls")),
     path('tinymce/', include('tinymce.urls')),
 ]
 
